0a877777de13f8a85635368284a52815192e74fd
RITTICK RAJ MISHRA <rittickrajmishra@gmail.com>
2025-08-08 14:56:29 +0530

    Add files via upload

---
+++ Halfpyramid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {         // loop for rows
+        for (j = 1; j <= i; j++) {     // loop for columns
+            printf("*");
+        }
+        printf("\n"); // move to next line
+    }
+
+    return 0;
+}
